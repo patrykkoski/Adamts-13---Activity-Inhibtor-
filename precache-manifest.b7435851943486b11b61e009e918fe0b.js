aae2e610de018943c68f4223842aad8c58600239
Patryk <patrykkoski1994@gmail.com>
2020-02-10 19:54:51 +0100

    Updates

---
+++ precache-manifest.b7435851943486b11b61e009e918fe0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c4fadf2270edea57e11173921c362f",
+    "url": "/adamts13/index.html"
+  },
+  {
+    "revision": "b6cd7c54e2eb3f285c0d",
+    "url": "/adamts13/static/css/main.6bfb73d5.chunk.css"
+  },
+  {
+    "revision": "9f253fe2baf1ad26169d",
+    "url": "/adamts13/static/js/2.c8c344a7.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/adamts13/static/js/2.c8c344a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cd7c54e2eb3f285c0d",
+    "url": "/adamts13/static/js/main.221034ad.chunk.js"
+  },
+  {
+    "revision": "c701e99fa02edcd93c8c",
+    "url": "/adamts13/static/js/runtime-main.725bb125.js"
+  },
+  {
+    "revision": "64045fac888c048f4761622cb9a44120",
+    "url": "/adamts13/static/media/bg.64045fac.jpg"
+  }
+]);
